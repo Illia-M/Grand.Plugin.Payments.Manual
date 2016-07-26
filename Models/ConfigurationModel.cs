1db38f94a85427be5ad94af6a3c136e2be8d5d3c
rafal kalisz <rafal.kalisz.dev@gmail.com>
2016-07-26 16:50:55 +0200

    init

---
+++ Models/ConfigurationModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Grand.Web.Framework;
+using Grand.Web.Framework.Mvc;
+
+namespace Grand.Plugin.Payments.Manual.Models
+{
+    public class ConfigurationModel : BaseNopModel
+    {
+        public string ActiveStoreScopeConfiguration { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.Manual.Fields.AdditionalFeePercentage")]
+        public bool AdditionalFeePercentage { get; set; }
+        public bool AdditionalFeePercentage_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.Manual.Fields.AdditionalFee")]
+        public decimal AdditionalFee { get; set; }
+        public bool AdditionalFee_OverrideForStore { get; set; }
+
+        public int TransactModeId { get; set; }
+        [NopResourceDisplayName("Plugins.Payments.Manual.Fields.TransactMode")]
+        public SelectList TransactModeValues { get; set; }
+        public bool TransactModeId_OverrideForStore { get; set; }
+    }
+}
